f038d758057a3b231a00aeb008d463604c38812d
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-10-29 19:16:11 +0200

    recaptcha - endpoint + form

--- prax2_google_recaptcha/server.js
+++ prax2_google_recaptcha/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,7 +12,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/register', (req, res) => {});
+app.post('/register', (req, res) => {
+  if (!req.body.captcha) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Будь-ласка оберіть каптчу' });
+  }
+
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${res.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  request(verifyUrl, (err, res, body) => {
+    body = JSON.parse(body);
+
+    if (!body.success) {
+      return res.status(403).json({
+        success: false,
+        message: 'Вибачте, каптча не буда верифікована',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Каптча пройшла',
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
