2a767ec1eedf27533ce183c998f0424380492f51
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-01 06:07:32 +0200

    blockchain - added transaction

--- prax3_blockchain/main.js
+++ prax3_blockchain/main.js
@@ -1,7 +1,8 @@
 const { BlockChain } = require('./modules/module.blockchain/blockChain');
+const { Transaction } = require('./modules/module.blockchain/transaction');
 const { saveData } = require('./modules/module.fs/fs_service');
 
 const blockChain = new BlockChain();
-blockChain.addBlock({ user: 'Andrew', money: 100 });
-blockChain.addBlock({ user: 'Dmytro', money: 200 });
+blockChain.addBlock(new Transaction('Andrew', 100));
+blockChain.addBlock(new Transaction('Dmytro', 200));
 console.dir(blockChain, { depth: null });
---
+++ prax3_blockchain/modules/module.blockchain/transaction.js
@@ -0,0 +1,8 @@
+class Transaction {
+  constructor(user, money) {
+    this.user = user;
+    this.money = money;
+  }
+}
+
+module.exports = { Transaction };
