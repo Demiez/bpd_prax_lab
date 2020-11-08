8d2c4fbc0c5c61027684c4301090a7f043164f40
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-08 15:33:31 +0200

    dh - client and server logic

---
+++ lab3_diffie-hellman/client/client.js
@@ -0,0 +1,191 @@
+const symmetric = require('../modules/symmetric.module');
+const DH = require('../modules/diffie.hellman.module');
+const yargs = require('yargs');
+const io = require('socket.io-client');
+const prompts = require('prompts');
+require('colors');
+
+// Аргументи командної строки
+const options = yargs
+  .usage('Usage: -i <ip-address> -p <server-port>')
+  .option('p', {
+    alias: 'port',
+    describe: 'Порт для конекту із сервером',
+    type: 'int',
+    demandOption: true,
+  })
+  .option('i', {
+    alias: 'ipAddress',
+    describe: 'IP адреса сервера (default: localhost)',
+    type: 'string',
+    demandOption: true,
+  }).argv;
+
+let server = options.ipAddress + ':' + options.port.toString();
+
+if (server.substring(0, 7) !== 'http://') {
+  server = 'http://' + server;
+}
+
+console.log('Підключаюсь до: ' + server);
+const socket = io(server);
+let currentChannel = 'plain-text';
+let userInputEnabled = false;
+let programStarted = false;
+
+const symmetricKey = symmetric.getSecretKey();
+let encryptionMethods = {
+  'plain-text': input => input,
+  'diffie-hellman': undefined,
+  'symmetric-encryption': input => symmetric.xorWithKey(input, symmetricKey),
+};
+
+socket.on('connect', () => {
+  console.log('\nІніціалізую загальний ключ із сервером');
+  if (programStarted) return;
+
+  socket.on('init-shared-key', data => {
+    const serverPrime = data.P;
+    const serverPRMP = data.G;
+    const serverPartialKey = data.A;
+
+    // Приватне число, яке використовуємо
+    const secretInt = DH.getSecretInt(serverPRMP);
+
+    // Створити та поділитися частковим ключем із сервером
+    const selfPartialKey = DH.computeKey(serverPrime, serverPRMP, secretInt);
+    socket.emit('partial-key', { B: selfPartialKey });
+
+    // Створити повний ключ
+    const diffieHellmanKey = DH.computeKey(
+      serverPrime,
+      serverPartialKey,
+      secretInt,
+    );
+
+    console.log(`Обчислений ключ ДХ: ${diffieHellmanKey}`);
+
+    // Встановити метод шифрування
+    encryptionMethods['diffie-hellman'] = input =>
+      symmetric.xorWithKey(input, diffieHellmanKey);
+
+    setUpSocketChannels(socket);
+    console.log('Для списку доступних команд наберіть ":help"');
+    userInputEnabled = true;
+    programStarted = true;
+    allowUserInput(socket);
+  });
+});
+
+function allowUserInput(socket) {
+  (async () => {
+    if (!userInputEnabled) {
+      setTimeout(() => allowUserInput(socket), 200);
+      return;
+    }
+    const input = await prompts({
+      type: 'text',
+      name: 'message',
+      message: 'Введіть повідомлення:',
+    });
+    userInputEnabled = false;
+    handleUserInput(socket, input.message);
+    allowUserInput(socket);
+  })();
+}
+
+const messagePrint = (message, switchInput, colour) => {
+  if (colour === undefined) colour = 'green';
+  if (userInputEnabled) {
+    setTimeout(() => messagePrint(message, switchInput), 200);
+    return;
+  }
+  console.log(message[colour]);
+  if (switchInput) {
+    userInputEnabled = true;
+  }
+};
+
+function handleUserInput(socket, input) {
+  if (checkForCommands(input)) {
+    return;
+  }
+  const encryptionMethod = encryptionMethods[currentChannel];
+  if (encryptionMethod === undefined) {
+    console.log('Вибачте, Діффі-Хеллман не встановлений');
+    currentChannel = 'plain-text';
+    handleUserInput(socket, input);
+  }
+  // Зашифруємо та передамо повідомлення
+  const encryptedInput = encryptionMethod(input);
+
+  // якщо включено щифрування - відображаємо зашифроване повідомлення
+  if (currentChannel === 'diffie-hellman') {
+    messagePrint(`${currentChannel} зашифроване повідомлення: ${encryptedInput}`, false, 'yellow');
+  }
+  socket.emit(currentChannel, { message: encryptedInput });
+}
+
+const changeChannel = newChannel => {
+  if (newChannel === currentChannel) {
+    messagePrint(`Вже використовую ${currentChannel}`, true, 'red');
+    return;
+  }
+  messagePrint(`Включено режим ${newChannel}`, true, 'cyan');
+  currentChannel = newChannel;
+};
+
+// Список доступних команд
+function checkForCommands(input) {
+  const flagMap = {
+    ':dh': 'diffie-hellman',
+    ':pt': 'plain-text',
+  };
+
+  const helpMessage =
+    'Доступні команди:' +
+    '\n\t:dh - режим шифрування Діффі-Хеллман - diffie-hellman' +
+    '\n\t:pt - режим простий текст без шифрування - plain-text' +
+    '\n\t:help - список команд' +
+    '\n\t:exit - вийти з програми';
+
+  if (input in flagMap) {
+    changeChannel(flagMap[input]);
+    return true;
+  }
+  const commandMap = {
+    ':help': () => {
+      messagePrint(helpMessage, true, 'yellow');
+    },
+    ':exit': () => {
+      console.log('Виходжу із програми...'.red);
+      process.exit(0);
+    },
+  };
+  if (!(input in commandMap)) {
+    return false;
+  }
+  commandMap[input]();
+  userInputEnabled = true;
+  return true;
+}
+
+// Встановлення сокет-каналів для шифрування та без шифрування
+function setUpSocketChannels(socket) {
+  // Без шифруванння
+  socket.on('plain-text', data => {
+    messagePrint(`Сервер (простий текст): ${data.text}`, true);
+  });
+
+  // Шифрування Діффі-Хеллман
+  socket.on('diffie-hellman', data => {
+    const response = data.text;
+    messagePrint(
+      `Отримано зашифроване повідомлення із сервера:\n${response}`,
+      false,
+      'yellow'
+    );
+    const decrypted = encryptionMethods['diffie-hellman'](response);
+    messagePrint(`Сервер (розшифровано): ${decrypted}`, true);
+  });
+}
---
+++ lab3_diffie-hellman/server/server.js
@@ -0,0 +1,111 @@
+const yargs = require('yargs');
+const symmetric = require('../modules/symmetric.module');
+const DH = require('../modules/diffie.hellman.module');
+
+// Аргументи командної стрічки
+const options = yargs
+  .usage('Usage: -p <server-port>')
+  .option('p', {
+    alias: 'port',
+    describe: 'Порт для підключення до сервера Діффі-Хеллмана',
+    type: 'int',
+    demandOption: true,
+  }).argv;
+
+console.log('Слухаю порт: ', options.port);
+
+const app = require('http').createServer(handler);
+const io = require('socket.io')(app);
+let gEaHIdx = 0;
+
+// Отримаємо варіанти відповіді сервера
+const serverResponseVariants = getServerResponseVariants().split('\n');
+const getGEaHLine = () => {
+  const line = serverResponseVariants[gEaHIdx];
+  gEaHIdx += 1;
+  if (gEaHIdx === serverResponseVariants.length) gEaHIdx = 0;
+  if (!line) return getGEaHLine();
+  return line;
+}
+
+// Завантажимо сим ключ із файла
+const symmetricKey = symmetric.getSecretKey('server');
+
+io.on('connection', socket => {
+  console.log(
+    `Підключився новий клієнт: ${socket.id}\nОчикую ініціалізацію Діффі-Хеллмана`,
+  );
+
+  // Процесимо рандомізований примітив
+  const prime = DH.getPrime();
+  const primRootModP = DH.getPrimitiveRoot(prime);
+  const secretInt = DH.getSecretInt(prime);
+
+  // Створимо частковий ключ для відправлення клієнту
+  const partialKey = DH.computeKey(prime, primRootModP, secretInt);
+
+  // Відправлення потрібної інформації клієнту
+  socket.emit('init-shared-key', {
+    P: prime,
+    G: primRootModP,
+    A: partialKey,
+  });
+
+  // Чекаємо на відповідь від клієнта, створимо свій ключ, канали комунікації
+  socket.on('partial-key', data => {
+    const clientPartial = data.B;
+    const computedDHKey = DH.computeKey(prime, clientPartial, secretInt);
+    console.log(`Інініціалізовано ключ: ${computedDHKey}, включаю канали`);
+    enableChannels(socket, computedDHKey);
+  });
+});
+
+app.listen(options.port);
+
+// для відображення в браузері
+function handler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('<h1>Алгоритм Діффі-Хеллмана</h1>');
+  res.end();
+}
+
+// Включити канали комунікації
+function enableChannels(socket, clientDHKey) {
+  // Без шифрування
+  socket.on('plain-text', data => {
+    console.log(`Повідомлення клієнта без шифрування: ${data.message}`);
+    socket.emit('plain-text', {
+      text: getGEaHLine(),
+    });
+  });
+
+  // Симетричне шифрування
+  socket.on('symmetric-encryption', data => {
+    const encryptedMessage = data.message;
+    const decryptedMessage = symmetric.xorWithKey(
+      encryptedMessage,
+      symmetricKey,
+    );
+    console.log(`Зашифроване повідомлення клієнта: ${encryptedMessage}`);
+    console.log(`Розшифроване повідомлення клієнта: ${decryptedMessage}`);
+    socket.emit('symmetric-encryption', {
+      text: symmetric.xorWithKey(getGEaHLine(), symmetricKey),
+    });
+  });
+
+  // Шифрування-розшифрування Діффі-Хеллман
+  socket.on('diffie-hellman', data => {
+    const encryptedMessage = data.message;
+    const decryptedMessage = symmetric.xorWithKey(
+      encryptedMessage,
+      clientDHKey,
+    );
+    console.log(`Зашифроване повідомлення (Діффі-Хеллман): ${encryptedMessage}`);
+    console.log(`Розшифроване повідомлення (Діффі-Хеллман): ${decryptedMessage}`);
+    socket.emit('diffie-hellman', { text: symmetric.xorWithKey(getGEaHLine(), clientDHKey) });
+  });
+}
+
+function getServerResponseVariants() {
+  return require('fs').readFileSync('data/server_response_variants', 'utf8');
+}
